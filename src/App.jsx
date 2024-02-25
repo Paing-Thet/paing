4f8a454fb3538376975f69f4c1db56c8ec0d405e
PaingThet <haixian.hex@gmail.com>
2024-02-26 01:24:50 +0630

    add profile

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css";
+import Profile from './components/Profile';
+
+function App() {
+    return(
+        <>
+        <h1>Hello React</h1>
+        <h2>Bello Beact</h2>
+        <Profile />
+        </>
+    )
+};
+
+export default App;
