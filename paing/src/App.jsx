0fc48364eb59dd4414b54a4dbfe879eafe3c2f88
PaingThet <haixian.hex@gmail.com>
2024-02-28 01:49:46 +0630

    add profile

---
+++ paing/src/App.jsx
@@ -0,0 +1,9 @@
+import Me from "./components/Me";
+
+export default function App() {
+  return (
+    <>
+      <Me />
+    </>
+  );
+}
---
+++ paing/src/components/Me.jsx
@@ -0,0 +1,12 @@
+import "../css/Me.css";
+export default function Me() {
+  return (
+    <>
+      <img
+        className="profilePic"
+        src="https://scontent-kul3-1.xx.fbcdn.net/v/t39.30808-1/410424963_122101681880152381_6199169249397266561_n.jpg?stp=c0.33.200.200a_dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=5740b7&_nc_ohc=gHtMq0SM4FcAX9X4DNH&_nc_ht=scontent-kul3-1.xx&oh=00_AfBMYrhQcnUS2pxCihpTPCJa6qaWlIgteiweIvTmDDtsbQ&oe=65E3482E"
+        alt=""
+      />
+    </>
+  );
+}
